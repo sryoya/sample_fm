0ba55b16783fdc7b38e9ec6b381351a3c5fd1ea8
sryoya <sryoya0814@gmail.com>
2018-04-30 21:32:38 +0900

    playlist contorollerをコメントアウト&hamlのNameエラーを回避

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :make_new_instance_of_playlist
+  # before_action :make_new_instance_of_playlist
 
-  def make_new_instance_of_playlist
-    @playlist = Playlist.new
-  end
+  # def make_new_instance_of_playlist
+  #   @playlist = Playlist.new
+  # end
 end
