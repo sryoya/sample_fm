1609711343a79faeb2d9f146948489f7cae5cbbc
sryoya <sryoya0814@gmail.com>
2018-04-30 16:38:33 +0900

    itunes apiの検索条件をUS→JPに変更

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def index
-    @results = ITunesSearchAPI.search(term: params[:term], country: "US", media: "music")
+    @results = ITunesSearchAPI.search(term: params[:term], country: "JP", media: "music")
     @search_keyword = params[:term] if @results.blank?
   end
 end
