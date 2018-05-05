a9b1c51ab3c1346a871942f0b903204d052cf5fd
sryoya <sryoya0814@gmail.com>
2018-05-06 02:07:52 +0900

    【バグ修正】検索処理を同期通信にしたことに合わせて、曲再生のjsの場所を移動

--- app/assets/javascripts/audio.js
+++ app/assets/javascripts/audio.js
@@ -1,8 +1,48 @@
+//曲を再生
 $(function(){
-  $("#myModal").on('click', function(){
-    $('#form-body').html("<%=j render 'modal' %>")
-    $('#form').modal('show')
+  $("audio").on('play', function(){
+    var now = $(this).attr("id");
   });
+});
+
+//次の曲を自動再生
+function playNextSong(next_scheduled_song_id){
+  var next_playing_id = next_scheduled_song_id - 0 + 1;
+  var next_audio = $('#song_num' + next_playing_id);
+  if(next_audio.length != 0){
+    next_audio[0].play();
+  }else if($("audio").length != 0){
+    playNextSong(next_playing_id)
+  }
+  $(this).parents("li.media.center-block").remove();
+};
+
+//曲が終了⇛そのまま次の曲を自動再生
+$(function(){
+  $("audio").on('ended', function(){
+    var next_playing_id = $(this).attr('id').match(/[0-9]+\.?[0-9]*/g)[0];
+    playNextSong(next_playing_id)
+  });
+});
+
+
+$(function(){
+  $("audio").on('play', function(){
+    var other_audio = $("audio").not(this);
+    $.each(other_audio, function(i, audio){
+      audio.pause();
+    });
+  });
+});
+
+
+//モーダル表示用
+// $(function(){
+//   $("#myModal").on('click', function(){
+//     $('#form-body').html("<%=j render 'modal' %>")
+//     $('#form').modal('show')
+//   });
+
   // $("#song_num").bind("ended", function(){
   // $(".btn-default").on('click', function(){
   //   alert("aaa")
@@ -11,7 +51,7 @@ $(function(){
   // });
   // //   if($("#m" + num2).get(0).ended){nextSong();}
   // });
-});
+// });
 // function nextSong(){
 //   $(function(){
 //     var num = $(".music").length;
@@ -34,15 +74,3 @@ $(function(){
 //     $("#" + num2).get(0).play();
 //   });
 // };
-
-$(function(){
-  $("audio").on('play', function(){
-    var now = $(this).attr("id");
-  });
-  // $("#song_num").bind("ended", function(){
-
-  //   if($("#m" + num2).get(0).ended){nextSong();}
-  // });
-});
-
-
