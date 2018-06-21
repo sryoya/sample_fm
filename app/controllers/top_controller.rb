0b111bb946442d063a707fc51e8533d3871bf86a
sryoya <sryoya0814@gmail.com>
2018-06-21 13:02:05 +0900

    define new method of tracks

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,5 @@
 class TopController < ApplicationController
   def index
+    @results = get_track_by_random
   end
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,21 @@
 class Track < ActiveRecord::Base
   belongs_to :playlist
+
+  def get_track(track_name)
+  end
+
+  def create_track(track)
+    Track.create(artist_name: track[artist_name], track_name: track[track_name], collection_name: track[collection_name], playlist_id: track[playlist_id], preview_url: track[preview_url], artwork_url: track[artwork_url],
+                itunes_artist_id: track[itunes_artist_id], itunes_track_id: track[itunes_track_id], itunes_collection_id: track[itunes_collection_id], collection_view_url: track[collection_view_url], track_view_url: track[track_view_url])
+  end
+
+  def search_and_save_only_first_track(search_term)
+    results = ITunesSearchAPI.search(term: search_term, country: "JP", media: "music")
+    Track.create(result[0])
+  end
+
+  def get_track_by_random(track_num)
+    tracks = Track.all.limit(50)
+  end
+
 end
